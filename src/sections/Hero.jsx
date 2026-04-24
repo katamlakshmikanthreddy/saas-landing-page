e87584467c2652c9ebc5ef532bb3787b862ecc8e
katamlakshmikanthreddy <kanthreddy132@gmail.com>
2026-04-24 12:06:47 +0530

    Surya bro

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              VIDEO EDITING
+              Bro
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              AMAZINGLY SIMPLE
+              SURYA BRO
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI Video Editor to be an easy to use, quick to learn, and
-              surprisingly powerful
+              No.1 Editor in the whole world bro, You are surprisingly wonderful in all departments of editing,
+              Please teach us some skills bro
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
               <Button icon="/images/zap.svg">Try it now</Button>
