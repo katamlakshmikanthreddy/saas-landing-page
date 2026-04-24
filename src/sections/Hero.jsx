442c9cbe3e8457e5437eec3117f5bffdbd92e43a
katamlakshmikanthreddy <kanthreddy132@gmail.com>
2026-04-24 12:02:43 +0530

    Back to normal

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,13 +8,14 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Baby
+              VIDEO EDITING
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              I Love You So Much Ra❤️❤️
+              AMAZINGLY SIMPLE
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              You are my one and only love, you are my part of my life bangaram
+              We designed XORA AI Video Editor to be an easy to use, quick to learn, and
+              surprisingly powerful
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
               <Button icon="/images/zap.svg">Try it now</Button>
