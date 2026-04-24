8298b0492184c184738dfc4cb7b7d5e8011b70b5
katamlakshmikanthreddy <kanthreddy132@gmail.com>
2026-04-24 12:12:00 +0530

    Surya BHAI

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Bro
+              BHAI
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              SURYA BRO
+              SURYA BHAI
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               No.1 Editor in the whole world bro, You are surprisingly wonderful in all departments of editing,
-              Please teach us some skills bro
+              Lover boy Surya
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
               <Button icon="/images/zap.svg">Try it now</Button>
